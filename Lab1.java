0363dd3a1eddfcbebedcdbb5b63bab5f26914acf
Maris-Angelique <34096881+marisangelique@users.noreply.github.com>
2023-10-22 15:50:55 -1000

    Add files via upload
    Using the format specifier %.2f within the print line, basic math and if statement to display the category of the BMI the user falls into

--- Lab1.java
+++ Lab1.java
@@ -1,47 +1,76 @@
-
 import java.util.*;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
 /**
  *
- * @author mwhite23
- * This program will:
- * 1.Ask the user for their name
- * 2.Ask the user for their height in feet and inches
- * 3.Converts height into inches
- * 4.Ask for the user's weight in pounds
+ * @author marisangelique
+ * This program:
+ * 1.Asks user for their name
+ * 2.Asks use for their height in feet and inches
+ * 3.Converts height to inches
+ * 4.Asks user for weight in pounds
  * 5.Calculates the BMI
- * 6.Outputs data nicely
- * 
+ * 6.Outputs the data
  */
 public class Lab1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {     
-        //Design intro output
-        System.out.println("This is a Body Mass Index Calculator Program");
-        System.out.println("You will be asked for your name, weight in pounds, and height in feet and inches");
-        System.out.println("***********************************************************************************************");
-        
-        //Declare values to store name, height, and weight
-        String name;
-        double height;
-        double weight;
-        
-        //Create way to collect users input
+    
+    public static void main(String[] args) {
+        //Open class to detect keyboard inputs;
         Scanner keyboard = new Scanner(System.in);
         
-        //Ask the user for their name and store it in the string variable
-        System.out.println("What is your name?");
+        //Create variable to hold name, height, converted height, and weight
+        String name;
+        int heightFeet;
+        double heightInches;
+        double newHeight;
+        double weight;
+        double bmi;
+        
+        //Display program name 
+        System.out.println("This is a Body Mass Index Calculator Program\n" 
+                            + "You will be asked for your name, weight in pounds, and height in feet and inches\n"
+                            + "******************************************************************************************");
+        //Asks user for their name and stores input in the name variable
+        System.out.print("Please enter your name:");
         name = keyboard.nextLine();
         
-        //
+        //Asks user for their weight in pounds and stores input in the weight variable
+        System.out.print("Please enter your weight in pounds: ");
+        weight = keyboard.nextDouble();
+        
+        //Asks user for their height in feet and inches then stores input in the height variable
+        System.out.print("Please enter your height (input the number of feet): ");
+        heightFeet = keyboard.nextInt();
+        
+        System.out.print("Please enter your height (input the number of inches): ");
+        heightInches = keyboard.nextDouble();
+        
+        //Converts height to inches
+        newHeight = ((heightFeet * 12)+ heightInches);
+        
+        //Calculates BMI and rounds to the hundreds place
+        bmi = (weight/(Math.pow(newHeight, 2))) * 703;
+        
+        //Greets and displays BMI
+        System.out.printf( "Hi " + name + " your BMI is %.2f", bmi);
+        //Create an if statement that displays where they are on the BMI scale 
+        if(bmi < 18.5){
+            System.out.println("\nYour BMI puts you in the underweight category.");
+        } else if( bmi >= 18.5 && bmi <= 24.9 ){
+            System.out.println("\nYour BMI puts you in the healthy category.");
+        }else if( bmi >= 25.0  && bmi <= 29.9 ){
+            System.out.println("\nYour BMI puts you in the overweight category.");
+        }else if( bmi >= 30.0 ){
+            System.out.println("\nYour BMI puts you in the obese category.");
+        }
+        
+        
+//        //Testing
+//        System.out.println(name + " is " + weight + " pounds" + " and " + heightFeet + "'" + heightInches + " tall!");
+//        System.out.println(name + "'s height is " + newHeight + " in inches");
+//        System.out.printf(name + " has the BMI of %.2f", bmi);
+        
+        
     }
     
 }
